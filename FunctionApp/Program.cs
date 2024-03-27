f8587ad7ac02b554007678c5184478db4d4f19ae
leejgdh <leejgdh@hotmail.com>
2024-03-27 16:24:10 +0900

    swagger 문서 설정 추가

--- FunctionApp/Program.cs
+++ FunctionApp/Program.cs
@@ -1,11 +1,15 @@
 using FunctionApp.Models.DAO;
 using FunctionApp.Services;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication(
@@ -19,6 +23,41 @@ var host = new HostBuilder()
         service.AddApplicationInsightsTelemetryWorkerService();
         service.ConfigureFunctionsApplicationInsights();
 
+        #region Swagger
+
+        service.AddSingleton<IOpenApiConfigurationOptions>(_ =>
+        {
+            var options = new OpenApiConfigurationOptions()
+            {
+                Info = new OpenApiInfo()
+                {
+                    Version = "1.0.0",
+                    Title = "Azure function default",
+                    Description = "this is hwida item api",
+                    TermsOfService = new Uri("https://github.com/Azure/azure-functions-openapi-extension"),
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "leejgdh",
+                        Email = "leejgdh@hwida.com",
+                        Url = new Uri("https://github.com/Azure/azure-functions-openapi-extension/issues"),
+                    },
+                    License = new OpenApiLicense()
+                    {
+                        Name = "MIT",
+                        Url = new Uri("http://opensource.org/licenses/MIT"),
+                    }
+                },
+                Servers = DefaultOpenApiConfigurationOptions.GetHostNames(),
+                OpenApiVersion = OpenApiVersionType.V2,
+                IncludeRequestingHostName = true,
+                ForceHttps = false,
+                ForceHttp = false,
+            };
+
+            return options;
+        });
+
+        #endregion
         // DbContext 
         service.AddDbContext<MyDbContext>(option =>
         option.UseSqlServer(
